92df78fdc3bb0a7366dcc329ae0324aa8014d8e5
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-06-28 18:32:53 +0300

    Imported sections and initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import styled from "styled-components"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Landing from "./components/Landing"
+import Projects from "./components/Projects"
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: cornflowerblue;
+`
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Landing />
+      <About />
+      <Projects />
+      <Contact />
+    </Container>
   )
 }
 
