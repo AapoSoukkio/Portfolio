cd17231ae89f8a9013d9602ad2b5651e799021b6
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-06-29 15:46:21 +0300

    Projects section initalization ready

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,96 @@
 import React from "react";
-import { styled } from "styled-components";
+import styled from "styled-components";
+
+//Note: Use short words in order to prevent the overflow problem
+//TODO: Find out the cause of this problem
+const data = [
+  "Web design",
+  "Development",
+  "AI",
+  "Fullstack",
+  "3D"
+];
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  justify-content: center;
+  position: relative;
+`;
+
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const ListItem = styled.li`
+  font-size: 90px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+  &::after {
+    content: attr(data-text); 
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: pink;
+    width: 0px;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    &::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%;
+          overflow: hidden;
+        }
+      }
+    }
+  }
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Projects = () => {
-    return (
-        <Section>Projects</Section>
-    )
-}
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} data-text={item}>
+                {item}
+              </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
+};
 
-export default Projects
+export default Projects 
