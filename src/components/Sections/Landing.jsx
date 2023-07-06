6e3713857770548ab6c6599928885eec71d69a27
Aapo Soukkio <aapo.soukkio@edu.savonia.fi>
2023-07-07 01:52:43 +0300

    Speaking animation added

--- src/components/Sections/Landing.jsx
+++ src/components/Sections/Landing.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styled  from 'styled-components'
+import React, { useEffect, useState } from "react";
+import styled, { keyframes, css } from "styled-components";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import MyAvatarV2 from "../3D-Components/MyAvatarV2";
-
+import Typist from "react-typist";
 
 const Section = styled.div`
   height: 100vh;
@@ -17,9 +17,16 @@ const Section = styled.div`
 const Container = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-  width: 1400px;
+  max-width: 1400px;
+  width: 100%;
+  padding: 0 20px;
   display: flex;
   justify-content: space-between;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const Left = styled.div`
@@ -28,9 +35,20 @@ const Left = styled.div`
   flex-direction: column;
   justify-content: center;
   gap: 20px;
+  max-width: 100%;
+
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+    align-items: center;
+  }
 `;
+
 const Title = styled.h1`
   font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 48px;
+  }
 `;
 
 const WhatWeDo = styled.div`
@@ -50,6 +68,10 @@ const Subtitle = styled.h2`
 const Desc = styled.p`
   font-size: 24px;
   color: lightgray;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
 const Button = styled.button`
@@ -64,54 +86,120 @@ const Button = styled.button`
 `;
 
 const Right = styled.div`
-  flex: 3; 
+  flex: 3;
   position: relative;
 `;
 
-const Img = styled.img`
-  width: 800px;
-  height: 600px;
-  object-fit: contain;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  animation: animate 2s infinite ease alternate;
+const typingAnimation = keyframes`
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+`;
 
-  @keyframes animate {
-    to{
-      transform: translateY(20px);
+const typingStyles = css`
+  &.typing {
+    &::after {
+      content: "";
+      display: inline-block;
+      vertical-align: bottom;
+      width: 0;
+      height: 1.1em;
+      animation: ${typingAnimation} 3s steps(40) infinite;
     }
   }
 `;
 
-const Landing = () => {
-    return (
-        <Section>
-        <Container>
-          <Left>
-            <Title>Something, something, something</Title>
-            <WhatWeDo>
-              <Line src="./img/line.png" />
-              <Subtitle>What I do</Subtitle>
-            </WhatWeDo>
-            <Desc>Description here... and something more</Desc>
-            <Button>Learn More</Button>
-          </Left>
-          <Right>
-            <Canvas>
-              <OrbitControls enableZoom={false} />
-              <ambientLight intensity={1} />
-              <directionalLight position={[3, 2, 1]} />
-              <MyAvatarV2 />
-            </Canvas>
-            {/* <Img src="./img/monkey.png" /> */}
-          </Right>
-        </Container>
-      </Section>
-    )
-}
+const Speak = styled.div`
+  width: 215px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  position: absolute;
+  top: 20px;
+  right: 430px;
+  color: black;
+  font-size: 14px;
+  font-weight: 300390;
+  overflow: hidden;
+  opacity: ${props => (props.visible  ? "1" : "0")};
 
-export default Landing
+  ${typingStyles}
+
+  @media only screen and (max-width: 768px) {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+`;
+
+const Landing = () => {
+  const [visited, setVisited] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+  const [showSpeak, setShowSpeak] = useState(true);
+
+  useEffect(() => {
+    const typingDelay = setTimeout(() => {
+      setIsTyping(true);
+    }, 1000);
+
+    return () => {
+      clearTimeout(typingDelay);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isTyping) {
+      const speakDelay = setTimeout(() => {
+        setShowSpeak(false);
+      }, 8000);
+
+      return () => {
+        clearTimeout(speakDelay);
+      };
+    }
+  }, [isTyping]);
+
+  useEffect(() => {
+    // Set visited to true when the component mounts
+    setVisited(true);
+  }, []);
+
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <Title>Something, something, something</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What I do</Subtitle>
+          </WhatWeDo>
+          <Desc>Description here... and something more</Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {visited && isTyping && (
+            <Speak className={isTyping ? "typing" : ""} visible={showSpeak}>
+              <Typist cursor={{ show: false }} avgTypingDelay={30}>
+                Hi! My name is Aapo Soukkio. Thanks for visiting my website 🙂
+                Take a good look around and if you like what you see drop me a message!
+              </Typist>
+            </Speak>
+          )}
+          <Canvas>
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <MyAvatarV2 />
+          </Canvas>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
+
+export default Landing;
